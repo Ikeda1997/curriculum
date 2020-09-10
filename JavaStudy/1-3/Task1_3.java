21cf193f5c95ef392ddc0a6d933405dc97e37ec6
池田康太郎 <69068230+Ikeda1997@users.noreply.github.com>
2020-09-10 19:52:46 +0900

    演算子（再提出）
    Task1_3.javaの再作成

--- JavaStudy/1-3/Task1_3.java
+++ JavaStudy/1-3/Task1_3.java
@@ -14,14 +14,14 @@ public class Task1_3 {
     public static void main(String[] args) {
 
         // ① 5 と 10 を加算した値を代入する、変数 plus を作成しなさい。
-        int plus = 5;
+        int plus = 5 + 10 ;
         // ② 20 から 7 を減算した値を代入する、変数 minus を作成しなさい。
-        int minus = 20;
+        int minus = 20 - 7;
         // ③下記の処理について、何をしているのかコメントを記入してください。
         // [ここへ記述]10と2をかけたを値を代入する、変数multiplyを作成している。
         int multiply = 10 * 2;
         // ④ 20 を 6 で割った余りを代入する、変数 remi を作成しなさい。
-        int remi = 20;
+        int remi = 20 % 6;
         // ⑤下記の処理について、何をしているのかコメントを記入してください
         // [ここへ記述]townに"有楽町",lineに"線"を代入している。
         String town = "有楽町";
@@ -30,10 +30,10 @@ public class Task1_3 {
         // ⑥ ⑤で作成した変数を連結させた値を代入する変数 train を作成しなさい。
         String train = town + line;
         // ⑦ 変数plus, minus, multiply, remi, train をそれぞれ出力しなさい。
-        System.out.println(plus += 10);
-        System.out.println(minus -= 7);
+        System.out.println(plus);
+        System.out.println(minus);
         System.out.println(multiply);
-        System.out.println(remi %= 6);
+        System.out.println(remi);
         System.out.println(train);
     }
 }
