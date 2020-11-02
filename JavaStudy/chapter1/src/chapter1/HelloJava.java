8f84259915a660f53263c1c96b6b7a7b311c083d
池田康太郎 <69068230+Ikeda1997@users.noreply.github.com>
2020-11-02 21:53:14 +0900

    課題提出テスト
    ２章2-1

---
+++ JavaStudy/chapter1/src/chapter1/HelloJava.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+public class HelloJava {
+	private String message;
+	
+	public HelloJava() {
+		message = "Hello, Java!" ;
+	}
+	public void print() {
+		System.out.println(message);
+	}
+
+}
