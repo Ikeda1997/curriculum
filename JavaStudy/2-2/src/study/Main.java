fb72b2ca10c67916a57e75b68f5907138282461a
池田康太郎 <69068230+Ikeda1997@users.noreply.github.com>
2020-11-04 21:17:33 +0900

    課題提出テスト
    2-2の作成

---
+++ JavaStudy/2-2/src/study/Main.java
@@ -0,0 +1,11 @@
+package study;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Account account = new Account("hoge",123,"123abc");
+		account.printAccountInfo();
+	}
+
+}
